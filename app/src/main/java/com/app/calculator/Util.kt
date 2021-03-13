9fe254d4af4d23e612a22cb2102b4d480376e299
jinkyu <wlsrb8993@gmail.com>
2021-03-13 21:43:33 +0900

    도움말 추가.

---
+++ app/src/main/java/com/app/calculator/Util.kt
@@ -0,0 +1,22 @@
+package com.app.calculator
+
+import android.app.AlertDialog
+import android.content.Context
+
+class Util {
+
+
+    //타이틀과 메세지를 보내면 alert를 띄워준다.
+     fun alert(view : Context, title: String, message: String) {
+
+        val builder = AlertDialog.Builder(view);
+
+        builder.setTitle(title).setMessage(message)
+
+        val alertDialog = builder.create()
+
+        alertDialog.show()
+
+
+    }
+}
